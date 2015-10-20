e7234b56efc74ae88791c2921b2ba8de2e9d4888
rouslankhayaouri <rouslan_kh@hotmail.com>
2015-10-20 14:41:31 +0200

    eerste commit werkend tot aan oef 6
    afgewerkt tot aan oefening 6

---
+++ BL/ContactManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using DAL;
+
+namespace BL
+{
+  public class ContactManager : IContactManager
+  {
+    private readonly IRepository contactRepository;
+    public void AddContact(string name, string streetAndNumber, short zipCode, string city, Gender gender, DateTime birthDay, string phone, string mobile)
+    {
+      Address adres = new Address
+      {
+        StreetAndNumber = streetAndNumber,
+        Zipcode = zipCode,
+        City = city,
+      };
+      Contact contact = new Contact
+      {
+        Name = name,
+        Gender = gender,
+        Birthday = birthDay,
+        Phone = phone,
+        Mobile = mobile,
+        Adress = adres
+      };
+      
+    }
+
+    public ContactManager()
+    {
+      contactRepository = new MemoryRepository();
+    }
+
+    public void ChangeContact(Contact contactToChange)
+    {
+      contactRepository.UpdateContact(contactToChange);
+    }
+
+    public IEnumerable<Category> GetAllCategories()
+    {
+      return contactRepository.ReadAllCatergories();
+    }
+
+    public IEnumerable<Contact> GetAllContacts()
+    {
+      return contactRepository.ReadAllContacts();
+    }
+
+    public IEnumerable<Contact> GetContactsForACategory(int categoryId)
+    {
+      return contactRepository.ReadAllContacts(categoryId);
+    }
+
+    public Contact ReadContact(int id)
+    {
+      return contactRepository.ReadContact(id);
+    }
+
+    public void RemoveContact(int id)
+    {
+      contactRepository.DeleteContact(id);
+    }
+  }
+}
